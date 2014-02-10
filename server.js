5b04b283929498f5dd78c4e88ee158b777de1527
Daniel Almaguer <danielalmaguer@gmail.com>
2014-02-10 09:59:00 -0800

    Added mongoose

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
