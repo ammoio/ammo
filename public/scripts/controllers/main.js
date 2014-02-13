efb7766592b565e93104bd920e3dd392811b6a2a
Daniel Almaguer <danielalmaguer@gmail.com>
2014-02-13 10:55:50 -0800

    Initial basic layout

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -3,8 +3,10 @@ angular.module('ammoApp')
   .controller('MainController', function($scope) {
     // $scope.welcome = "ammo";
     $scope.songs = [];
+    $scope.showing = true;
 
     $scope.add = function(userInput) {
       $scope.songs.push({name: userInput});
+      $scope.showing = !$scope.showing;
     };
   });
