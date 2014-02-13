448cf82c694c294f4fe49f1eb7b2f49acd0b3026
Alex's Chromebook <alexmckenley@gmail.com>
2014-02-12 20:12:00 -0800

    added playlist schema to schema.js

--- db/schema.js
+++ db/schema.js
@@ -1,10 +1,10 @@
 /*
-*Queues
-*
-*Queues are live playlists, they contain metadata pertaining to the 
-*current state of the queue such as song. These are not associated
-*with any user and are completely public.
-*
+ *Queue
+ *
+ *Queues are live playlists, they contain metadata pertaining to the 
+ *current state of the queue such as song. These are not associated
+ *with any user and are completely public.
+ *
 */
 
 var Queue = {
@@ -14,4 +14,18 @@ var Queue = {
   lastAccessed: Date,
   songs: Array,
   currentSong: Number,
+};
+
+/*
+ *Playlist
+ *
+ *Playlists are simple song lists. They simply provide a way for users to save lists
+  of songs. You can create a playlist from a queue or turn a queue into a playlist.
+ *
+*/
+
+var Playlist = {
+  name: String,
+  songs: Array
+  //private: Boolean //This could possibly be implemented to create private playlists.
 };
