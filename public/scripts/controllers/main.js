73d4e27c27b035682736b50b0b83dd643e847d5a
Ray Ma <rhm262@gmail.com>
2014-02-13 12:12:05 -0800

    Added share button.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -5,8 +5,21 @@ angular.module('ammoApp')
     $scope.songs = [];
     $scope.showing = true;
 
+    $scope.share = function() {
+      console.log('sharing');
+    };
+
     $scope.add = function(userInput) {
-      $scope.songs.push({name: userInput});
+      /************** query sound cloud *************/
+      //SC is initialized in app.js
+      //q: query string
+      //limit: number of results to return
+      SC.get('/tracks', { q: userInput, limit: 3 }, function(tracks) {
+        for (var i = 0; i < tracks.length; i++) {
+          console.log(tracks[i]);
+          $scope.songs.push({name: tracks[i].title, id: tracks[i].id});
+        }
+      });
       $scope.showing = !$scope.showing;
     };
   });
