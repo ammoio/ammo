ba73e7c6c7baad705cc48b03f9af7e609f691bf9
Daniel Almaguer <danielalmaguer@gmail.com>
2014-02-13 12:56:54 -0800

    Added youtube search basic functionality

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,12 +1,25 @@
 angular.module('ammoApp')
   
-  .controller('MainController', function($scope) {
+  .controller('MainController', function($scope, $http, ParseService) {
     // $scope.welcome = "ammo";
     $scope.songs = [];
     $scope.showing = true;
+    $scope.searchResults = [];
 
     $scope.add = function(userInput) {
       $scope.songs.push({name: userInput});
       $scope.showing = !$scope.showing;
-    };
+
+      $http({ method: 'GET', url: 'https://gdata.youtube.com/feeds/api/videos?q=' + userInput + '&orderby=relevance&max-results=5&alt=json&v=2' })
+        .then(function(results) {
+          console.log(results);
+          results.data.feed.entry.forEach(function(video) { 
+            $scope.searchResults.push({
+              title: video.title.$t,
+              service: "youtube",
+              service_id: ParseService.youtube(video.link[0].href)
+            });
+          });
+        });
+    };    
   });
