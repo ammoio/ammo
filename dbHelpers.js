2f016a480676121fc3ccc69908cca95a39afc091
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-13 21:02:04 -0800

    added createUser function

--- dbHelpers.js
+++ dbHelpers.js
@@ -10,6 +10,8 @@ var Models = require('./models');
 mongoose.connect('mongodb://localhost/ammo');
 
 module.exports = {
+
+/* ======== Queue Helpers ======== */
   getQueues: function(){
     var d = Q.defer();
     Models.Queue.find(function(err, data){
@@ -54,7 +56,9 @@ module.exports = {
     Models.Queue.findOne({shareId: id}, function(err, model){
       for (var key in obj) {
         if( obj.hasOwnProperty(key) ) {
-          model[key] = obj[key];
+          if(key !== "shareId"){
+            model[key] = obj[key];
+          }
         }
       }
       model.markModified('songs');
@@ -73,7 +77,7 @@ module.exports = {
     var d = Q.defer();
     Models.Queue.findOne({shareId: id}, function(err, model){
       if(Array.isArray(song)){
-        model.songs.concat(song);
+        model.songs = model.songs.concat(song);
       } else {
         model.songs.push(song);
       }
@@ -88,8 +92,114 @@ module.exports = {
     });
 
     return d.promise;
-  }
+  },
 
+
+/* ======== Playlist Helpers ======== */
+  getUserPlaylists: function(username){
+    var d = Q.defer();
+    Models.User.findOne({username: username}, function(err, user){
+      if(err){
+        d.reject(err);
+      } else {
+        d.resolve(user.playlists);
+      }
+    });
+    return d.promise;
+  },
+
+  getUserPlaylist: function(username, id){
+    var d = Q.defer();
+    Models.User.findOne({username: username}, function(err, user){
+      if(err){
+        d.reject(err);
+      } else {
+        d.resolve(user.playlists[id]);
+      }
+    });
+    return d.promise;
+  },
+
+  createPlaylist: function(username, playlist){
+    var d = Q.defer();
+    playlist.id = crypto.randomBytes(4).toString('hex');
+    Models.User.findOne({username: username}, function(err, user){
+      if(err){
+        d.reject(err);
+      } else {
+        user.playlists[playlist.id] = playlist;
+        user.markModified("playlists");
+        user.save(function(err, user){
+          d.resolve(playlist);
+        });
+      }
+    });
+    return d.promise;
+  },
+
+  updatePlaylist: function(username, id, playlist){
+    var d = Q.defer();
+    Models.User.findOne({username: username}, function(err, user){
+      if(err){
+        d.reject(err);
+      } else {
+        for (var key in playlist) {
+          if( playlist.hasOwnProperty(key) ) {
+            if(key !== "id"){
+              user.playlists[id][key] = playlist[key];
+            }
+          }
+        }
+        user.markModified("playlists");
+        user.save(function(err, user){
+          if(err){
+            d.reject(err);
+          } else {
+            d.resolve(playlist);
+          }
+        });
+      }
+    });
+    return d.promise;
+  },
+
+  addSongToPlaylist: function(username, id, song){
+    var d = Q.defer();
+    Models.User.findOne({username: username}, function(err, user){
+      if(err){
+        d.reject(err);
+      } else {
+        if(Array.isArray(song)){
+          user.playlists[id].songs = user.playlists[id].songs.concat(song);
+        } else {
+          user.playlists[id].songs.push(song);
+        }
+        user.markModified("playlists");
+        user.save(function(err, user){
+          if(err){
+            d.reject(err);
+          } else {
+            d.resolve(song);
+          }
+        });
+      }
+    });
+    return d.promise;
+  },
+
+  createUser: function(user){
+    var d = Q.defer();
+    var newUser = new Models.User(user);
+    newUser.save(function(err, user){
+      if (err) {
+        d.reject(err);
+      } else {
+        d.resolve(user);
+      }
+    });
+
+    return d.promise;
+  }
 
 };
 
