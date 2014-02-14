c301c82f33410ddcd015d26459cc8907bd4c07e1
Hack Reactor Students <github@hackreactor.com>
2014-02-13 18:40:08 -0800

    Cleaned up whitespace in models.js

--- models.js
+++ models.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 /*
  *Queue
  *
- *Queues are live playlists, they contain metadata pertaining to the 
+ *Queues are live playlists, they contain metadata pertaining to the
  *current state of the queue such as song. These are not associated
  *with any user and are completely public.
  *
