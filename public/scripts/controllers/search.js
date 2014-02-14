8aa3f0b7879afc3692abe8b8e7ec75c6e32c4b09
Ray Ma <rhm262@gmail.com>
2014-02-14 14:31:26 -0800

    Added comments

--- public/scripts/controllers/search.js
+++ public/scripts/controllers/search.js
@@ -2,7 +2,18 @@ angular.module('ammoApp')
   .controller('SearchController', function($scope, $location, SearchService, QueueService) {
     //set searchResults on scope to reflect change in view
     $scope.searchResults = SearchService.searchResults;
+    
+    /*
+      ========== addToQueue ==========
+      Enqueue song and change path to home to switch controller.
+      Called when addToQueue clicked in search.html
 
+      Params: 
+        param1: song (object)
+          - a single song object
+
+      Return: No return
+    */
     $scope.addToQueue = function(song) {
       QueueService.enqueue(song);
       $location.path('/');      
