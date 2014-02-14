ea6e60fc3e28f7c8907b15c44caff40379f772f8
Ray Ma <rhm262@gmail.com>
2014-02-14 13:30:40 -0800

    minor comments

--- public/scripts/controllers/search.js
+++ public/scripts/controllers/search.js
@@ -1,4 +1,9 @@
 angular.module('ammoApp')
   .controller('SearchController', function($scope, SearchService) {
+    //set searchResults on scope to reflect change in view
     $scope.searchResults = SearchService.searchResults;
+
+    $scope.addToQueue = function() {
+
+    };
   });
