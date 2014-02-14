ca74377e34224b81b9dd15da4569a33d68e78bcb
Hack Reactor Students <github@hackreactor.com>
2014-02-13 18:01:34 -0800

    deleted schema.js, migrated info to models.js

--- models.js
+++ models.js
@@ -1,5 +1,14 @@
 var mongoose = require('mongoose');
 
+
+/*
+ *Queue
+ *
+ *Queues are live playlists, they contain metadata pertaining to the 
+ *current state of the queue such as song. These are not associated
+ *with any user and are completely public.
+ *
+*/
 var queueSchema = mongoose.Schema({
   name: String,
   shareId: String,
@@ -9,11 +18,25 @@ var queueSchema = mongoose.Schema({
   currentSong: Number
 });
 
+/*
+ *Playlist
+ *
+ *Playlists are simple song lists. They simply provide a way for users to save lists
+  of songs. You can create a playlist from a queue or turn a queue into a playlist.
+ *
+*/
 var playlistSchema = mongoose.Schema({
   name: String,
   songs: []
 });
 
+
+/*
+ *Song
+ *
+ *Songs can have many different proprities depending on the service, but should include the following
+ *
+*/
 var songSchema = mongoose.Schema({
   url: String,
   service: String,
