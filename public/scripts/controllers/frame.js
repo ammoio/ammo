fb70cc1b458a25602b4e66c2ab9f90519cf7e60b
Ray Ma <rhm262@gmail.com>
2014-02-14 11:10:30 -0800

    Refactored searchResults into SearchService. Added dependencies for SearchService. Added player.play(song) in search.html on click.

--- public/scripts/controllers/frame.js
+++ public/scripts/controllers/frame.js
@@ -1,14 +1,5 @@
-angular.module('ammoApp')
-
+angular.module('ammoApp') 
   .controller('FrameController', function($scope, $http, ParseService, $location, SearchService, QueueService) {
-    $scope.songs = [];
-    $scope.searchResults = []; //maybe redundant
-
-
-    $scope.add = function(userInput) {
-      $scope.songs.push({name: userInput});
-    };
-
 
     /* 
       ========== $scope.search ==========
@@ -19,30 +10,15 @@ angular.module('ammoApp')
           - Whathever is currently on the search box (inside form #search)
     */
     $scope.search = function(userInput) {
-      // Clearing past search results
-      $scope.searchResults = [];
       //Call SearchService for each of the services and pass pushResults as a callback 
-      SearchService.youtube(userInput, pushResults);
-      SearchService.soundcloud(userInput, pushResults); 
+      SearchService.youtube(userInput);
+      SearchService.soundcloud(userInput); 
       $location.path('/search');
     };
 
     /* Share Button: when clicked, share button do a post request to /queues */
     $scope.share = function() {
       QueueService.saveQueue($scope.searchResults);
-    };
-
-    /* 
-      ========== pushResults ==========
-      Gets called as a callback from SearchService to be able to push results
-      to $scope.searchResults since they are in different scopes.
-
-      Params: 
-        song
-          - song object returned by SearchService $http 'GET' request
-    */
-    var pushResults = function(song) {
-      $scope.searchResults.push(song);
     };
 
   });
