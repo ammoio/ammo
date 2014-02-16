9c9f67ebb44d0c3122e20b6f7082fae0911b9848
Ray Ma <rhm262@gmail.com>
2014-02-15 16:33:05 -0800

    Option to tweet the link.

--- public/scripts/controllers/queue.js
+++ public/scripts/controllers/queue.js
@@ -1,17 +1,15 @@
 angular.module('ammoApp')
 
-  .controller('QueueController', function($scope, QueueService) {
+  .controller('QueueController', function($scope, $location, QueueService) {
     
-    $('#myModal').modal('show');
-
     QueueService.getQueue().then(function(queue){ //Sets the scopes songs to the current q from qservice
+      $scope.shareId = queue.shareId;
       $scope.songs = queue.songs;
     });
 
     /*
       ========== share ==========
       -Triggered from a click on the "share" button. Displays modal to prompt for playlist name and passphrase.
-        When modal is submitted, trigger QueueService.saveQueue with those inputs.
 
       Params:
         None
@@ -21,17 +19,30 @@ angular.module('ammoApp')
 
     $scope.share = function() {
       $('#shareRequestModal').modal();
-      $('.shareNow').on('click', function() {
-        QueueService.saveQueue($scope.queueName, $scope.passphrase)
-        .then(function(queue) {
-          $('#shareResponseModal').modal();
-          $('.tweetLink').on('click', function() {
-              
-          });
-        });
-      });
     };
 
+    
+    /*
+      ========== shareRequestModal ==========
+      -Called when shareRequestModal is filled out and "Share" is clicked. When modal is submitted, trigger QueueService.saveQueue with those inputs.
+
+      Params:
+        None
+
+      Return: No return
+    */
+    $scope.shareRequestModal = function() {
+      QueueService.saveQueue($scope.queueName, $scope.passphrase)
+      .then(function(queue) {
+        $('#shareResponseModal').modal(); //show response modal
+        $scope.shareLink = 'http://localhost/share/' + queue.shareId;
+        $('.twitter-share-button').attr({
+        'data-url': $scope.shareLink,
+        'data-text': "Hey, checkout this playlist I made!\n"
+        }); //dynamically set the url
+      });
+    };
+    
     /*
       ========== passToPlay ==========
       -Triggered from an ng-click on a song in the queue. Takes an index, sets it as the current song index, 
