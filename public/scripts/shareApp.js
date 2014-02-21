897cacd36a6c2c7f0692f8bc788b5b32043ce5df
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-20 18:03:30 -0800

    work on seperating share view

---
+++ public/scripts/shareApp.js
@@ -0,0 +1,30 @@
+angular.module('ammoApp', ['ngRoute', 'ui.bootstrap', 'ngCookies'])
+
+  .config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/q/:id', {
+        templateUrl: '/views/share.html',
+        controller: 'ShareController'
+      })
+      .when('/search', {
+        templateUrl: '/views/search.html',
+        controller: 'SearchController'
+      });
+
+    OAuth.initialize('YTaWoCjSvB9X8LcCyc8hn6sp798'); 
+
+  })
+
+  .run(function ($rootScope, $location) {
+    var history = [];
+
+    $rootScope.$on('$routeChangeSuccess', function() {
+      history.push($location.$$path);
+    });
+
+    $rootScope.back = function () {
+      var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+      $location.path(prevUrl);
+    };
+  });
