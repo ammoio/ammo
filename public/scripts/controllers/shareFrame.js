3143ce068a43a7303d5510c7892f4b1605af83b6
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-28 14:45:45 -0800

    linted shareFrameController

--- public/scripts/controllers/shareFrame.js
+++ public/scripts/controllers/shareFrame.js
@@ -1,5 +1,5 @@
 angular.module('ammoApp')
-  .controller('FrameController', function($q, $scope, $http, $location, $cookies, ParseService, SearchService, UserService, QueueService) {
+  .controller('FrameController', function ($q, $scope, $location, ParseService, SearchService, UserService, QueueService) {
 
     /*************** scope variables ***************/
     $scope.UserService = UserService;
@@ -17,12 +17,17 @@ angular.module('ammoApp')
       Checks if the input is a url. If so, parse url in ParseService, else call SearchService
       to search each API
     */
-    $scope.search = function(userInput) {
+    $scope.search = function (userInput) {
+
+      var isUrl = function (s) {
+        var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        return regexp.test(s);
+      };
       //Call SearchService for each of the services
       $scope.userInput = ""; // clearing the input box
       $scope.searching = true;
 
-      if(isUrl(userInput)) {
+      if (isUrl(userInput)) {
         ParseService.parseURL(userInput);
         $scope.searching = false;
       } else {
@@ -31,24 +36,20 @@ angular.module('ammoApp')
           SearchService.youtube(userInput),
           SearchService.soundcloud(userInput)
         ])
-        .then(function(results) {
-          results.forEach(function(result) {
-            SearchService.searchResults = SearchService.searchResults.concat(result);
+          .then(function (results) {
+            results.forEach(function (result) {
+              SearchService.searchResults = SearchService.searchResults.concat(result);
+            });
+            $scope.searching = false;
           });
-          $scope.searching = false;
-        });
       }
       $location.path('/search');
     };
 
-    //$scope.search helper
-    var isUrl = function isUrl(s) {
-      var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-      return regexp.test(s);
-    };
+
 
     /* Share Button: when clicked, share button do a post request to /queues */
-    $scope.share = function() {
+    $scope.share = function () {
       QueueService.saveQueue($scope.searchResults);
     };
 
@@ -56,38 +57,37 @@ angular.module('ammoApp')
       ========== $scope.shareRequestModal ==========
       -Called when shareRequestModal is filled out and "Share" is clicked. When modal is submitted
     */
-    $scope.shareRequestModal = function() {
+    $scope.shareRequestModal = function () {
       QueueService.saveQueue($scope.queueName)
-      .then(function(queue) {
-        $('#shareResponseModal').addClass('md-show'); //show response modal
-      });
+        .then(function () {
+          $('#shareResponseModal').addClass('md-show'); //show response modal
+        });
     };
 
 
     /* ========== $scope.showQueue ==========
       Redirects the user to /listen to load the queue
     */
-    $scope.showQueue = function() {
+    $scope.showQueue = function () {
       $location.path('/listen');
     };
 
     /* ========== $scope.closeModal ==========
       hide all the modals
     */
-    $scope.closeModal = function (selector){
+    $scope.closeModal = function () {
       $('.md-show').removeClass('md-show');
     };
 
     /* ========== $scope.fixTime ==========
       format the time from seconds for display
     */
-    $scope.fixTime = function(seconds) {
-      var mins = seconds / 60 | 0; 
+    $scope.fixTime = function (seconds) {
+      var mins = seconds / 60 | 0;
       seconds = seconds % 60 | 0;
-      if(seconds < 10) {
+      if (seconds < 10) {
         return mins + ":0" + seconds;
-      } else {
-        return mins + ":" + seconds;
       }
+      return mins + ":" + seconds;
     };
   });
