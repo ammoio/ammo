5bd4e9739b31341bd813116e509027f191345be7
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-28 12:09:47 -0800

    linted queue_helpers

--- app/controllers/queue_controllers.js
+++ app/controllers/queue_controllers.js
@@ -7,97 +7,97 @@ var socketServer = require('../sockets');
 
 module.exports = {
 
-  getQueues: function(req, res){
+  getQueues: function (req, res) {
     Queue.getQueues()
-    .then(function(queues){
-      res.send(queues);
-    })
-    .fail(function (err) {
-      res.send(500, err);
-    });
+      .then(function (queues) {
+        res.send(queues);
+      })
+      .fail(function (err) {
+        res.send(500, err);
+      });
   },
 
-  getQueue: function(req, res){
+  getQueue: function (req, res) {
     Queue.getQueue(req.params.id)
-    .then(function(queue){
-      res.send(queue);
-    })
-    .fail(function (err) {
-      res.send(500, err);
-    });
+      .then(function (queue) {
+        res.send(queue);
+      })
+      .fail(function (err) {
+        res.send(500, err);
+      });
   },
 
   createQueue: function (req, res) {
     Queue.createQueue(req.body)
-    .then(function(queue){
-      res.send(queue);
-    })
-    .fail(function (err) {
-      res.send(500, err);
-    });
+      .then(function (queue) {
+        res.send(queue);
+      })
+      .fail(function (err) {
+        res.send(500, err);
+      });
   },
 
-  addSong: function(req, res){
+  addSong: function (req, res) {
     loginHelpers.isAuthorized(req.params.id, req.cookies['ammoio.sid'])
-    .then(function(){
-      return Queue.addSongToQueue(req.params.id, req.body);
-    })
-    .then(function(song){
-      res.send(song);
-    })
-    .fail(function (err) {
-      console.log(err);
-      if(err === "not logged in"){
-        res.send(401);
-      } else {
-        res.send(500, err);
-      }
-    });
+      .then(function () {
+        return Queue.addSongToQueue(req.params.id, req.body);
+      })
+      .then(function (song) {
+        res.send(song);
+      })
+      .fail(function (err) {
+        console.log(err);
+        if (err === "not logged in") {
+          res.send(401);
+        } else {
+          res.send(500, err);
+        }
+      });
   },
 
-  update: function(req, res){
-     loginHelpers.isAuthorized(req.params.id, req.cookies['ammoio.sid'])
-    .then(function(){
-      return Queue.updateQueue(req.params.id, req.body);
-    })
-    .then(function(queue){
-      socketServer.emit('updateView', {shareId: queue.shareId});
-      res.send(queue);
-    })
-    .fail(function (err) {
-      if(err === "not logged in"){
-        res.send(401);
-      } else {
-        res.send(500, err);
-      }
-    });
-  },
-
-  deleteSong: function(req, res){
+  update: function (req, res) {
     loginHelpers.isAuthorized(req.params.id, req.cookies['ammoio.sid'])
-    .then(function(){
-      return Queue.removeSongFromQueue(req.params.id, req.params.index);
-    })
-    .then(function(song){
-      res.send(song);
-    })
-    .fail(function (err) {
-      if(err === "not logged in"){
-        res.send(401);
-      } else {
-        res.send(500, err);
-      }
-    });
+      .then(function () {
+        return Queue.updateQueue(req.params.id, req.body);
+      })
+      .then(function (queue) {
+        socketServer.emit('updateView', {shareId: queue.shareId});
+        res.send(queue);
+      })
+      .fail(function (err) {
+        if (err === "not logged in") {
+          res.send(401);
+        } else {
+          res.send(500, err);
+        }
+      });
+  },
+
+  deleteSong: function (req, res) {
+    loginHelpers.isAuthorized(req.params.id, req.cookies['ammoio.sid'])
+      .then(function () {
+        return Queue.removeSongFromQueue(req.params.id, req.params.index);
+      })
+      .then(function (song) {
+        res.send(song);
+      })
+      .fail(function (err) {
+        if (err === "not logged in") {
+          res.send(401);
+        } else {
+          res.send(500, err);
+        }
+      });
   },
 
   renderShareIndex: function (req, res) {
     Queue.getQueue(req.params.id)
-    .then(function(queue){
-      res.sendfile('/public/shareIndex.html', {root: __dirname + '/../..'});  
-    })
-    .fail(function (err) {
-      res.redirect('/');
-    });
+      .then(function () {
+        res.sendfile('/public/shareIndex.html', {root: __dirname + '/../..'});
+      })
+      .fail(function () {
+        res.redirect('/');
+      });
   }
 
 };
