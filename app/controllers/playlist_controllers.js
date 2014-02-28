3341d3cd5bf45c0f7d243071800847caaa95ea97
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-28 12:04:38 -0800

    linted playlist_controllers

--- app/controllers/playlist_controllers.js
+++ app/controllers/playlist_controllers.js
@@ -6,35 +6,35 @@ var Queue = require('../models/queue_model');
 
 module.exports = {
 
-  getUserPlaylists: function(req, res){
+  getUserPlaylists: function (req, res) {
     User.getPlaylists(req.params.user)
-    .then(function(playlists){
-      res.send(playlists);
-    })
-    .fail(function (err) {
-      res.send(500, err);
-    });
+      .then(function (playlists) {
+        res.send(playlists);
+      })
+      .fail(function (err) {
+        res.send(500, err);
+      });
   },
 
-  create: function(req, res){
+  create: function (req, res) {
     loginHelpers.validateSession(req.params.user, req.cookies['ammoio.sid'])
-    .then(function (response) {
-      console.log("Valid Session", req.params.user);
-      return true;
-    })
-    .then(function () {
-      return User.createPlaylist(req.params.user, req.body);
-    })
-    .then(function(playlist){
-      res.send(playlist);
-    })
-    .fail(function (err) {
-      if(err === "not logged in"){
-        res.send(401);
-      } else {
-        res.send(500, err);
-      }
-    });
+      .then(function () {
+        console.log("Valid Session", req.params.user);
+        return true;
+      })
+      .then(function () {
+        return User.createPlaylist(req.params.user, req.body);
+      })
+      .then(function (playlist) {
+        res.send(playlist);
+      })
+      .fail(function (err) {
+        if (err === "not logged in") {
+          res.send(401);
+        } else {
+          res.send(500, err);
+        }
+      });
   }
 
 };
