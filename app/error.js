0b1c62a6551e0422a44cbe1ab4b757eb9f1e98a6
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-28 12:30:19 -0800

    linted error.js

--- app/error.js
+++ app/error.js
@@ -1,7 +1,7 @@
 /* ======== Express Error Handler Middleware ========*/
 
-module.exports = function(err, req, res, next){
-  if(err) {
+module.exports = function (err, req, res, next) {
+  if (err) {
     console.error(err);
   } else {
     return next();
