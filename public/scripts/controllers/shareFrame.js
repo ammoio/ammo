4d8b461eb7990f5d750db3d8b35dbc2816433dbf
Ray Ma <rhm262@gmail.com>
2014-02-27 21:01:21 -0800

    Fix bug with select song not adding class correctly

--- public/scripts/controllers/shareFrame.js
+++ public/scripts/controllers/shareFrame.js
@@ -9,31 +9,13 @@ angular.module('ammoApp')
     $scope.isMobile = window.innerWidth <= 800 && window.innerHeight <= 600;
     $scope.socket = io.connect($scope.location.host()); //initializing socket
 
-    $http({ method: 'GET', url: '/user'})
-      .success(function(user) {
-        UserService.setUser(user);
-        UserService.setLogged(true);
-      })
-      .error(function(err) {
-        console.log(err);
-      });
-
-    $scope.isLogged = function() {
-      return UserService.isLogged();
-    };
-
-
-    // This variable is used to know when youtube
-    // and deezer are loaded ($scope.stopLoadingBar())
-    $scope.assetsLoaded = 0;
+    /*************** $scope functions ***************/
 
     /*
       ========== $scope.search ==========
       Gets called when user clicks or hits enter on the search bar/button
-
-      Params:
-        userInput
-          - Whathever is currently on the search box (inside form #search)
+      Checks if the input is a url. If so, parse url in ParseService, else call SearchService
+      to search each API
     */
     $scope.search = function(userInput) {
       //Call SearchService for each of the services
@@ -58,7 +40,8 @@ angular.module('ammoApp')
       }
       $location.path('/search');
     };
-
+    
+    //$scope.search helper
     var isUrl = function isUrl(s) {
       var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
       return regexp.test(s);
@@ -69,68 +52,17 @@ angular.module('ammoApp')
       QueueService.saveQueue($scope.searchResults);
     };
 
-
-    $scope.login = function() {
-      if(UserService.user.loggedIn) {
-        $http({ method: 'GET', url: '/logout'})
-        .success(function(){
-          $cookies.sessionId = "";
-          UserService.logout();
-        })
-        .error(function(){
-          console.log("error logging out");
-        });
-      } else {
-        $cookies.sessionId = guid();
-        OAuth.popup('facebook', { state: $cookies.sessionId }, function(err, res) {
-          if(err) {
-            console.log(err);
-            return;
-          }
-
-          $http({ method: 'POST', url: '/login', data: { code: res.code }})
-            .success(function(userObj) {
-              UserService.setUser(userObj);
-              UserService.setLogged(true);
-
-              $http.get(userObj.username + "/playlists")
-                .success(function(playlists) {
-                  UserService.user.playlists = playlists;
-              });
-            })
-            .error(function(err){
-              console.log(err);
-            });
-        });
-      }
-    };
-
     /*
-      ========== shareRequestModal ==========
-      -Called when shareRequestModal is filled out and "Share" is clicked. When modal is submitted, trigger QueueService.saveQueue with those inputs.
-
-      Params:
-        None
-
-      Return: No return
+      ========== $scope.shareRequestModal ==========
+      -Called when shareRequestModal is filled out and "Share" is clicked. When modal is submitted
     */
     $scope.shareRequestModal = function() {
       QueueService.saveQueue($scope.queueName)
       .then(function(queue) {
-        $('#shareResponseModal').modal(); //show response modal
+        $('#shareResponseModal').addClass('md-show'); //show response modal
       });
     };
 
-
-    /* ========== $scope.changePlaylist ==========
-      Redirects user to /playlist/:id who then will display the tracks of that playlist
-
-      Params:
-        playlist: playlist object getting passed when a user clicks to a playlist name on the sidebar
-    */
-    $scope.changePlaylist = function(playlist) {
-      $location.path('/playlist/' + playlist.shareId);
-    };
 
     /* ========== $scope.showQueue ==========
       Redirects the user to /listen to load the queue
@@ -140,11 +72,16 @@ angular.module('ammoApp')
     };
 
     
+    /* ========== $scope.closeModal ==========
+      hide all the modals
+    */
     $scope.closeModal = function (selector){
-      console.log('in directive');
       $('.md-show').removeClass('md-show');
-    }
+    };
 
+    /* ========== $scope.fixTime ==========
+      format the time from seconds for display
+    */    
     $scope.fixTime = function(seconds) {
       var mins = seconds / 60 | 0; 
       seconds = seconds % 60 | 0;
