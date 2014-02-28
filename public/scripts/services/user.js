aaddd6b125f41da02311b58379e575290684216d
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-28 15:14:04 -0800

    linted user Service

--- public/scripts/services/user.js
+++ public/scripts/services/user.js
@@ -1,6 +1,6 @@
 angular.module('ammoApp')
 
-  .service('UserService', function($http, $cookies, $q) {
+  .service('UserService', function ($http, $cookies, $q) {
     this.user = {
       username: null,
       name: null,
@@ -9,19 +9,19 @@ angular.module('ammoApp')
       playlists: null
     };
 
-    this.setUser = function(userObj) {
+    this.setUser = function (userObj) {
       this.user = userObj;
     };
 
-    this.setLogged = function(status) {
+    this.setLogged = function (status) {
       this.user.loggedIn = status;
     };
 
-    this.isLogged = function() {
+    this.isLogged = function () {
       return this.user.loggedIn;
     };
 
-    this.logout = function() {
+    this.logout = function () {
       this.user.username = null;
       this.user.name = null;
       this.user.email = null;
@@ -29,25 +29,25 @@ angular.module('ammoApp')
       this.user.playlists = null;
     };
 
-    this.verifyUser = function() {
+    this.verifyUser = function () {
       var that = this;
       $http({ method: 'GET', url: '/user'})
-      .success(function(user) {
-        that.setUser(user);
-        that.setLogged(true);
-      })
-      .error(function(err) {
-        console.log(err);
-      });
+        .success(function (user) {
+          that.setUser(user);
+          that.setLogged(true);
+        })
+        .error(function (err) {
+          console.log(err);
+        });
     };
 
-    this.login = function() {
+    this.login = function () {
       var that = this;
-      if(this.user.loggedIn) {
+      if (this.user.loggedIn) {
         that.logOutRequest();
       } else {
-        OAuth.popup('facebook', { state: $cookies['ammoio.sid'] }, function(err, res) {
-          if(err) {
+        OAuth.popup('facebook', { state: $cookies['ammoio.sid'] }, function (err, res) {
+          if (err) {
             console.log(err);
             return;
           }
@@ -58,17 +58,17 @@ angular.module('ammoApp')
     };
 
     /* login helpers */
-    this.logOutRequest = function() {
+    this.logOutRequest = function () {
       var that = this;
       $http({ method: 'GET', url: '/logout/' + that.user.username})
-      .success(function(){
-        that.logout();
-      })
-      .error(function(){
-        console.log("error logging out");
-      });
+        .success(function () {
+          that.logout();
+        })
+        .error(function () {
+          console.log("error logging out");
+        });
     };
-    this.userQuery = function(res) {
+    this.userQuery = function (res) {
       var that = this;
       // POST to /login with Facebook response code
       $http({
@@ -76,26 +76,26 @@ angular.module('ammoApp')
         url: '/login',
         data: { code: res.code }
       })
-      // set the user on success
-      .success(function(userObj) {
-        that.setUser(userObj);
-        that.setLogged(true);
-        that.getUserPlaylists(userObj);
-      })
-      .error(function(err){
-        console.log(err);
-      });
+        // set the user on success
+        .success(function (userObj) {
+          that.setUser(userObj);
+          that.setLogged(true);
+          that.getUserPlaylists(userObj);
+        })
+        .error(function (err) {
+          console.log(err);
+        });
     };
 
-    this.getUserPlaylists = function(userObj) {
+    this.getUserPlaylists = function (userObj) {
       var d = $q.defer();
       var that = this;
       $http.get("/" + userObj.username + "/playlists")
-        .success(function(playlists) {
+        .success(function (playlists) {
           that.user.playlists = playlists;
           d.resolve(playlists);
         })
-        .error(function(err){
+        .error(function (err) {
           d.reject(err);
         });
 
