82449a24ab91f8d31cd078a45575bf9109d633dd
Alex's HR Computer <alexmckenley@gmail.com>
2014-02-27 21:20:29 -0800

    Correclty redirecting to playlist view after creation

--- public/scripts/services/playlist.js
+++ public/scripts/services/playlist.js
@@ -1,6 +1,8 @@
 angular.module('ammoApp')
-.service('PlaylistService', ['$http', 'UserService', function($http, UserService) {
+.service('PlaylistService', ['$http', 'UserService', '$q', function($http, UserService, $q) {
   this.saveToPlaylist = function(name) {
+    var d = $q.defer();
+
     if(!UserService.user.loggedIn) {
       alert("Can't save playlist. Please log in with Facebook");
       return;
@@ -10,20 +12,33 @@ angular.module('ammoApp')
       alert("Playlist name can not be empty");
       return;
     }
-    postPlaylist(name);
+    postPlaylist(name).then(function(playlist){
+      d.resolve(playlist);
+    })
+    .catch(function(err){
+      d.reject(err);
+    });
+
+    return d.promise;
   };
 
   var postPlaylist = function(name) {
+    var d = $q.defer();
+
     var playlistObj = {
       name: name,
-      songs: [] 
+      songs: []
     };
     $http({ method: 'POST', url: '/' + UserService.user.username + '/playlists', data: playlistObj })
     .success(function(data) {
       UserService.user.playlists.push(data);
+      d.resolve(data);
     })
     .error(function() {
       console.log("Error saving playlist");
+      d.reject();
     });
+
+    return d.promise;
   };
 }]);
