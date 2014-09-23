0ce02128984221c4cbebb53398c2eedc5f873307
Alex Taylor <alexmckenley@gmail.com>
2014-09-22 19:02:37 -0700

    Create new public dir with base file layout

---
+++ public/app.js
@@ -0,0 +1,36 @@
+(function (){
+    'use strict';
+
+    angular.module('ammo', [
+        'ui.router',
+        'ammo.controllers.header',
+        'ammo.controllers.sidebar',
+        'ammo.controllers.home',
+        'ammo.controllers.player'
+    ])
+        .config(config);
+
+    function config($stateProvider){
+        $stateProvider
+            .state('ammo',{
+                views: {
+                    'header': {
+                        templateUrl: 'header/header.tpl.html',
+                        controller: 'HeaderController as HeaderController'
+                    },
+                    'sidebar': {
+                        templateUrl: 'sidebar/sidebar.tpl.html',
+                        controller: 'SidebarController as SidebarController'
+                    },
+                    'main': {
+                        templateUrl: 'home/home.tpl.html',
+                        controller: 'HomeController as HomeController'
+                    },
+                    'player': {
+                        templateUrl: 'player/player.tpl.html',
+                        controller: 'PlayerController as PlayerController'
+                    }
+                }
+            });
+    }
+})();
