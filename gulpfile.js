a8efac83461cd82b121c0d2ef10fc12c3b8307fb
Alex Taylor <alexmckenley@gmail.com>
2014-09-23 22:02:03 -0700

    Remove sourcemaps and fix indentation in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,41 @@
 // dependencies
-var gulp = require('gulp');
-var git = require('gulp-git');
-var bump = require('gulp-bump');
-var filter = require('gulp-filter');
-var tag_version = require('gulp-tag-version');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var concatCss = require('gulp-concat-css');
-var templateCache = require('gulp-angular-templatecache');
-var mainBowerFiles = require('main-bower-files');
+var gulp = require('gulp'),
+    git = require('gulp-git'),
+    bump = require('gulp-bump'),
+    filter = require('gulp-filter'),
+    tag_version = require('gulp-tag-version'),
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    templateCache = require('gulp-angular-templatecache'),
+    mainBowerFiles = require('main-bower-files');
 
 
 //Paths
 var paths = {
-    scripts: ['public/**/*.js'],
-    styles: ['public/**/*.css'],
-    html: ['public/**/*.html', '!public/index.html'],
-    index: ['public/index.html']
+  scripts: ['public/**/*.js'],
+  styles: ['public/**/*.css'],
+  html: ['public/**/*.html', '!public/index.html'],
+  index: ['public/index.html']
 };
 
 /**
  * Compile templates for use in the templateCache.
  */
 gulp.task('html', function () {
-    gulp.src(paths.html)
-        .pipe(templateCache('templates.js', {
-            standalone: true,
-            module: 'ammo.templates'
-        }))
-        .pipe(gulp.dest('build/js/'));
+  gulp.src(paths.html)
+    .pipe(templateCache('templates.js', {
+      standalone: true,
+      module: 'ammo.templates'
+    }))
+    .pipe(gulp.dest('build/js/'));
 });
 
 /**
  * Move index file to the build dir.
  */
 gulp.task('index', function () {
-    gulp.src(paths.index)
-        .pipe(gulp.dest('build/'));
+  gulp.src(paths.index)
+    .pipe(gulp.dest('build/'));
 });
 
 /**
@@ -44,11 +43,7 @@ gulp.task('index', function () {
  */
 gulp.task('scripts', function() {
   gulp.src(paths.scripts)
-    // Uncomment the following lines to enable sourcemaps for concatenated files.
-    // This doesnt effect us right now because we arent minifying.
-    //.pipe(sourcemaps.init())
     .pipe(concat('bundle.js'))
-    //.pipe(sourcemaps.write())
     .pipe(gulp.dest('build/js/'));
 });
 
@@ -57,10 +52,8 @@ gulp.task('scripts', function() {
  */
 gulp.task('vendor-scripts', function() {
 gulp.src(mainBowerFiles(), { base: 'bower_components' })
-    //.pipe(sourcemaps.init())
-      .pipe(concat('vendor.js'))
-    //.pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/js/'));
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest('build/js/'));
 });
 
 /**
