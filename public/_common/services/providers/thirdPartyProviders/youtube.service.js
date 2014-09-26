e28f183fa8b54011348ecb329ee764578db43fbb
Alex's Chromebook <alexmckenley@gmail.com>
2014-09-25 22:15:08 -0700

    Move thirdPartyProviders into providers folder

---
+++ public/_common/services/providers/thirdPartyProviders/youtube.service.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ammo.services.providers.youtube', [])
+    .factory('youtubeService', youtubeService);
+
+    function youtubeService() {
+      var service = {};
+
+      return service;
+    }
+})();
