0e98060e5abcee298147da45909057b9abfbbf62
Alex's Chromebook <alexmckenley@gmail.com>
2014-09-25 23:05:51 -0700

    Added jsdoc comments to eventService

--- public/_common/services/event/event.service.js
+++ public/_common/services/event/event.service.js
@@ -15,21 +15,36 @@
       return service;
 
       ////////////
-      function publish(name, args) {
+      /**
+       * @name publish
+       * @param {String} event to publish
+       * @param args Data to be passed to all listeners of the event
+       */
+      function publish(event, args) {
         //there are no listeners
-        if (!$rootScope.$$listeners[name]) {
+        if (!$rootScope.$$listeners[event]) {
           return;
         }
 
         //send the event
-        $rootScope.$broadcast(name, args);
+        $rootScope.$broadcast(event, args);
       }
 
-      function subscribe(name, callback) {
-        return $rootScope.$on(name, callback);
+      /**
+       * @name subscribe
+       * @param {String} event to subscribe to
+       * @param {Function} callback to be called when an event is published
+       * @return {Function}
+       */
+      function subscribe(event, callback) {
+        return $rootScope.$on(event, callback);
       }
 
-      //Pass in the result of subscribe to this method, or just call the method returned from subscribe to unsubscribe
+      /**
+       * @name unsubscribe
+       * @desc Pass in the result of subscribe to this method, or just call the method returned from subscribe to unsubscribe
+       * @param {Function} handle function returned from the subscribe method
+       */
       function unsubscribe(handle) {
         if (angular.isFunction(handle)) {
           handle();
