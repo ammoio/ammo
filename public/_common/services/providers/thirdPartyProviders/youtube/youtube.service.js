eac9c785ec691b643b3ba97f86431ee04897022c
Daniel Almaguer <danielalmaguer@gmail.com>
2014-09-27 18:12:48 -0700

    Added comments

--- public/_common/services/providers/thirdPartyProviders/youtube/youtube.service.js
+++ public/_common/services/providers/thirdPartyProviders/youtube/youtube.service.js
@@ -24,43 +24,76 @@
       return service;
 
       ////////////
+      /**
+       * Loads a song to the youtube player and then plays it
+       * @params {object} song Song to play
+      **/
       function play(song) {
         loadPlayer()
           .then(function successLoadPlayer() {
             youtube.loadVideoById(song.serviceId, 0, 'large');
             youtube.playVideo();
+          }, function errorLoadPlayer() {
+            eventService.publish('error', 'Failed to load YouTube Player');
           });
       }
 
+      /**
+       * Pause the youtube player
+       */
       function pause() {
         youtube.pauseVideo();
       }
 
+      /**
+       * Unpause the youtube player, this is different than the play function since we
+       * are not loading the song to the player in this function.
+       */
       function unPause() {
         youtube.playVideo();
       }
 
-      function seekTo(seconds) {
-        youtube.seekTo(seconds, true);
+      /**
+       * Seek the youtube player to a specific second
+       * @param {integer} second The time in seconds to a specific part of the video
+       */
+      function seekTo(second) {
+        youtube.seekTo(second, true);
       }
 
+      /**
+       * Stop the youtube player
+       */
       function stop() {
         youtube.stopVideo();
       }
 
+      /**
+       * Mute the youtube player
+       */
       function mute() {
         youtube.mute();
       }
 
+      /**
+       * Unmute the youtube player
+       */
       function unMute() {
         youtube.unMute();
       }
 
-      // volume = 0 to 100
+      /**
+       * Change the volume of the youtube player
+       * @param {integer} volume A number from 0 to 100 to set the volume to
+       */
       function setVolume(volume) {
         youtube.setVolume(volume);
       }
 
+      /**
+       * Loads the youtube iframe API asynchronously
+       * @returns {promise} Used for knowing when the youtube player is ready or failed to load.
+       */
       function loadPlayer() {
         var youtubeScript,
             youtubeElement,
@@ -84,20 +117,35 @@
         return deferred.promise;
 
         ///////////
+        /**
+         * Sets a timer to reject the promise if the youtube api is taking too long
+         * @returns {$timeout}
+         */
         function setRejectTimer() {
           return $timeout(function() {
             deferred.reject();
-            eventService.publish('error', 'Failed to load YouTube Player');
           }, 7000);
         }
 
+        /**
+         * The youtube iframe API requires us to register some functions on the global scope. This function
+         * sets those functions on the $window.
+         */
         function setWindowVariables() {
+
+          /**
+           * Triggered when the youtube player is ready. Cancels the timer and resolves the loadPlayer() promise.
+           */
           $window.onPlayerReady = function() {
             isPlayerLoaded = true;
             $timeout.cancel(rejectTimer);
             deferred.resolve();
           };
 
+          /**
+           * Triggered when the youtube player changes state. Publishes an event according to the new state.
+           * @param {object} event The new state event
+           */
           $window.onPlayerStateChange = function(event) {
             var newEvent = {
               service: 'youtube'
@@ -119,6 +167,10 @@
             }
           };
 
+          /**
+           * Triggered when the API finishes loading, sets the functions that we want to call on different events,
+           * and create a youtube player.
+           */
           $window.onYouTubeIframeAPIReady = function() {
             youtube = new YT.Player('youtube', {
               height: '200',
