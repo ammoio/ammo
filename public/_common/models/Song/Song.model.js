f01fd4687f42bd96201eacd34ccd236d483e4b45
Alex's Chromebook <alexmckenley@gmail.com>
2014-11-20 00:53:42 -0800

    remove redundant song methods

--- public/_common/models/Song/Song.model.js
+++ public/_common/models/Song/Song.model.js
@@ -6,22 +6,8 @@
     .factory('Song', songModel);
 
   function songModel(Restangular, providers) {
-    var model = Restangular.service('songs'),
-        methods = {
-          play: play
-        };
+    var model = Restangular.service('songs');
 
-    Restangular.extendModel('songs', function(obj) {
-      return angular.extend(obj, methods);
-    });
-
-    return angular.extend(model, methods);
-
-    ////////////
-    function play(song) {
-      song = song || this;
-
-      event.publish('play', song);
-    }
+    return model;
   }
 })();
