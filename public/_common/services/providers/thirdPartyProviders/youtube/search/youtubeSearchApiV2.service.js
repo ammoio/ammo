6b9553faf17a466bfca0c8c96e476bf6d9373285
Daniel Almaguer <danielalmaguer@gmail.com>
2014-12-02 23:13:10 -0800

    Added youtube API v2 search and youtube search restructure

---
+++ public/_common/services/providers/thirdPartyProviders/youtube/search/youtubeSearchApiV2.service.js
@@ -0,0 +1,103 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ammo.youtube.searchApiV2.service', [])
+    .factory('youtubeSearchApiV2', youtubeSearchApiV2Service);
+
+  function youtubeSearchApiV2Service($http, $q, $timeout) {
+    var service;
+
+    service = {
+      search: search
+    };
+
+    return service;
+
+    ///////////
+    /**
+     * @param {string} query Query user typed on the search box
+     * @param {number} limit Limit the number of search results (integer)
+     * @returns {promise} Promise that will resolve to an array of song objects
+     */
+    function search(query, limit) {
+      var deferred = $q.defer(),
+          rejectTimer = setRejectTimer();
+
+      getSearchResults()
+        .then(function youtubeSearchSuccess(results) {
+          var videos;
+
+          $timeout.cancel(rejectTimer);
+
+          videos = results.data.data.items;
+
+          if (!videos) {
+            deferred.reject();
+          }
+
+          return videos;
+        })
+        .then(function youtubeVideos(videos) {
+          deferred.resolve(createSongObjects(videos));
+        })
+        .catch(function youtubeSearchError() {
+          deferred.reject();
+        });
+
+      return deferred.promise;
+
+      ///////////
+      /**
+       * Search on youtube with the query the user typed
+       * @returns {promise}
+       */
+      function getSearchResults() {
+        return $http.get('http://gdata.youtube.com/feeds/api/videos',
+          {
+            params: {
+              'v': 2,
+              'alt': 'jsonc',
+              'max-results': limit,
+              'q': query
+            }
+          });
+      }
+
+      /**
+       * Creates the array of song objects with the API data
+       * @param {array} videos Array of returned videos from API
+       * @returns {array} Array of song objects
+       */
+      function createSongObjects(videos) {
+        var songs = [];
+
+        _.each(videos, function (video) {
+          var name = video.title.split(' - ');
+
+          songs.push({
+            artist: name.length > 1 ? name[0] : null,
+            title: name.length > 1 ? name[1] : name[0],
+            duration: video.duration,
+            service: 'youtube',
+            serviceId: video.id,
+            url: 'http://youtu.be/' + video.id,
+            image: video.thumbnail.hqDefault
+          });
+        });
+
+        return songs;
+      }
+
+      /**
+       * Sets a timer to reject the promise if the youtube api is taking too long
+       * @returns {$timeout}
+       */
+      function setRejectTimer() {
+        return $timeout(function() {
+          deferred.resolve([]);
+        }, 5000);
+      }
+    }
+  }
+}());
--- public/javascript/_common/services/providers/thirdPartyProviders/youtube/search/youtubeSearch.service.js
+++ public/javascript/_common/services/providers/thirdPartyProviders/youtube/search/youtubeSearch.service.js
@@ -2,12 +2,14 @@
   'use strict';
 
   angular
-    .module('ammo.youtube.search.service', [])
+    .module('ammo.youtube.search.service', [
+      'ammo.youtube.searchApiV2.service',
+      'ammo.youtube.searchApiV3.service'
+    ])
     .factory('youtubeSearch', youtubeSearchService);
 
-  function youtubeSearchService($http, $q, $timeout) {
-    var apiKey = 'AIzaSyCsNh0OdWpESmiBBlzjpMjvbrMyKTFFFe8', // ToDo: Seed the api key
-        service;
+  function youtubeSearchService(youtubeSearchApiV2, youtubeSearchApiV3) {
+    var service;
 
     service = {
       search: search
@@ -17,137 +19,20 @@
 
     ///////////
     /**
+     * We use this function to decide if we want to search on YouTube's API V2 or V3.
      * @param {string} query Query user typed on the search box
      * @param {number} limit Limit the number of search results (integer)
+     * @param {number} version YouTube API version
      * @returns {promise} Promise that will resolve to an array of song objects
      */
-    function search(query, limit) {
-      var deferred = $q.defer(),
-          rejectTimer = setRejectTimer();
-
+    function search(query, limit, version) {
       limit = limit || 5;
+      version = version || 2;
 
-      getSearchResults()
-        .then(function youtubeSearchSuccess(results) {
-          var videos = results.data.items;
-
-          if (_.isEmpty(videos)) {
-            deferred.resolve([]);
-            return;
-          }
-          return requestDetails(videos);
-        })
-
-        .then(function youtubeDetailsSuccess(results) {
-          var videos;
-          $timeout.cancel(rejectTimer);
-
-          if (_.isUndefined(results)) {
-            return;
-          }
-          videos = results.data.items;
-
-          if (_.isEmpty(videos)) {
-            deferred.resolve([]);
-            return;
-          }
-          createSongObjects(videos);
-        })
-
-        .catch(function youtubeSearchError() {
-          deferred.reject();
-        });
-      return deferred.promise;
-
-
-      ///////////
-      /**
-       * Search on youtube with the query the user typed
-       * @returns {promise}
-       */
-      function getSearchResults() {
-        return $http.get('https://www.googleapis.com/youtube/v3/search',
-          {
-            params: {
-              part: 'snippet',
-              maxResults: limit,
-              q: query,
-              type: 'video',
-              videoCategoryId: 10, // Music Category
-              key: apiKey
-            }
-          }
-        );
-      }
-      /**
-       * Request more details from the videos returned from the search result since the search API won't return
-       * all we need.
-       * @param {array} videos Array of video objects
-       * @returns {promise}
-       */
-      function requestDetails(videos) {
-        var videoIds;
-
-        videoIds = _.map(videos, function(video) {
-          return video.id.videoId ;
-        });
-        videoIds = videoIds.join(',');
-
-        return $http.get('https://www.googleapis.com/youtube/v3/videos', {
-          params: {
-            part: 'snippet,contentDetails',
-            id: videoIds,
-            key: apiKey
-          }
-        });
-      }
-
-      /**
-       * Creates the array of song objects with the API data
-       * @param {array} videos Array of detailed video objects
-       */
-      function createSongObjects(videos) {
-        var songs,
-            name;
-
-        songs = _.map(videos, function(video) {
-          name = video.snippet.title.split(' - ');
-
-          return {
-            artist: name.length > 1 ? name[0] : null,
-            title: name.length > 1 ? name[1] : name[0],
-            duration: timeToSeconds(video.contentDetails.duration),
-            service: 'youtube',
-            serviceId: video.id,
-            url: 'http://youtu.be/' + video.id,
-            image: video.snippet.thumbnails.high.url
-          };
-        });
-        deferred.resolve(songs);
-      }
-
-      /**
-       * Sets a timer to reject the promise if the youtube api is taking too long
-       * @returns {$timeout}
-       */
-      function setRejectTimer() {
-        return $timeout(function() {
-          deferred.resolve([]);
-        }, 5000);
-      }
-
-      /**
-       * Youtube return the video duration in ISO 8601 durations (e.g: PT10M46S), this function returns
-       * the time in seconds
-       * @param time Time in ISO 8601 format
-       * @returns {number} time in seconds (integer)
-       */
-      function timeToSeconds(time) {
-        var hours = time.match(/(\d+)(?=[H])/ig) || [0],
-            minutes = time.match(/(\d+)(?=[M])/ig) || [0],
-            seconds = time.match(/(\d+)(?=[S])/ig) || [0];
-
-        return (parseInt(hours, 10) * 60 * 60) + parseInt(minutes, 10) * 60 + parseInt(seconds, 10);
+      if (version === 2) {
+        return youtubeSearchApiV2.search(query, limit);
+      } else {
+        return youtubeSearchApiV3.search(query, limit);
       }
     }
   }
